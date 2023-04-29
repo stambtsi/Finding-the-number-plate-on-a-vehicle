16f0a3790655cc492d1c6323e36804e5405eb7b8
Tsimafei Stambrouski <timastmb@gmail.com>
2023-04-29 15:11:38 +0200

    The second commit. Number plate location accuracy has been improved to around 85 per cent. Main addition: bitmask AND with the threshold of the original image.

--- find_location_nb_plate.py
+++ find_location_nb_plate.py
@@ -15,10 +15,30 @@ class PyImageSearchANPR:
                 cv2.waitKey(0)
 
     def locate_license_plate_candidates(self, gray, keep=5):
-        rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (17, 9))
+        rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (19, 9))
 
         blur = cv2.GaussianBlur(gray, (3, 3), 0)
+        mask = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        self.debug_imshow("TMP", mask)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, rectKern)
+        self.debug_imshow("Mask", mask)
 
+        # gradX = cv2.Sobel(thresh, ddepth=cv2.CV_16S, dx=1, dy=0)
+        # gradY = cv2.Sobel(thresh, ddepth=cv2.CV_16S, dx=0, dy=1)
+        #
+        # abs_grad_x = cv2.convertScaleAbs(gradX)
+        # abs_grad_y = cv2.convertScaleAbs(gradY)
+        #
+        # grad = cv2.bitwise_or(abs_grad_x, abs_grad_y)
+        # self.debug_imshow("Grad tmp", grad)
+        # grad = cv2.GaussianBlur(grad, (5, 5), 0)
+        # grad = cv2.dilate(grad, rectKern, iterations=2)
+        # grad = cv2.erode(grad, rectKern, iterations=2)
+        # self.debug_imshow("MORPH_CLOSE tMP", grad)
+
+        # ---------------------------------------------------------------------------------
+
+        blur = cv2.GaussianBlur(gray, (3, 3), 0)
         gradX = cv2.Sobel(blur, ddepth=cv2.CV_16S, dx=1, dy=0)
         gradY = cv2.Sobel(blur, ddepth=cv2.CV_16S, dx=0, dy=1)
 
@@ -30,8 +50,8 @@ class PyImageSearchANPR:
         self.debug_imshow("Grad", grad)
 
         # grad = cv2.morphologyEx(grad, cv2.MORPH_CLOSE, rectKern)
-        grad = cv2.dilate(grad, rectKern, iterations=1)
-        grad = cv2.erode(grad, rectKern, iterations=1)
+        grad = cv2.dilate(grad, rectKern, iterations=2)
+        grad = cv2.erode(grad, rectKern, iterations=2)
         self.debug_imshow("MORPH_CLOSE", grad)
         # thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         thresh = cv2.threshold(grad, 20, 255, cv2.THRESH_BINARY_INV)[1]
@@ -40,12 +60,19 @@ class PyImageSearchANPR:
         # thresh = cv2.threshold(blur, 210, 255, cv2.THRESH_OTSU )[1]
         # self.debug_imshow("Thresh", thresh)
 
-        thresh = cv2.erode(thresh, rectKern, iterations=2)
-        thresh = cv2.dilate(thresh, rectKern, iterations=2)
-        self.debug_imshow("Thresh Opened", thresh)
+        min_rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 5))
+        thresh1 = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, min_rectKern)
+        # thresh1 = cv2.dilate(thresh, rectKern, iterations=1)
+        # thresh1 = cv2.erode(thresh1, rectKern, iterations=1)
+        # thresh1 = cv2.erode(thresh1, rectKern, iterations=2)
+        # thresh1 = cv2.dilate(thresh1, rectKern, iterations=2)
+        self.debug_imshow("Thresh Opened", thresh1)
 
-        edged = cv2.Canny(thresh, 30, 200)
-        self.debug_imshow("Edged", edged)
+        masked = cv2.bitwise_and(thresh1, thresh1, mask=mask)
+        self.debug_imshow("Masked", masked)
+
+        edged = cv2.Canny(masked, 30, 200)
+        # self.debug_imshow("Edged", edged)
 
         contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         # contours = imutils.grab_contours(contours)
@@ -55,9 +82,14 @@ class PyImageSearchANPR:
         # print(contours)
 
         # Draw all contours
+        # tmp_cont = []
+        # for i in contours:
+        #     if len(i) < 10:
+        #         tmp_cont.append(i)
+        # print(type(contours[0]))
+        # print(type(tmp_cont[0]))
         cv2.drawContours(gray, contours, -1, (0, 255, 0), 3)
-        cv2.imshow('Contours', gray)
-        # cv2.waitKey(0)
+        self.debug_imshow('Contours Gray', gray)
 
         return contours
 
@@ -70,7 +102,7 @@ class PyImageSearchANPR:
             print('ar : ', ar)
             if self.minAR <= ar <= self.maxAR:
                 success = True
-                cv2.rectangle(image, (x-5, y-5), (x+w+5, y+h), (0, 255, 0), 2)
+                cv2.rectangle(image, (x - 5, y - 5), (x + w + 5, y + h), (0, 255, 0), 2)
                 cv2.imshow('Contours', image)
                 break
         cv2.waitKey(0)
@@ -87,6 +119,5 @@ class PyImageSearchANPR:
 
 anpr = PyImageSearchANPR(debug=False)
 
-
-img = cv2.imread('images/0.jpg')
+img = cv2.imread('images/5.jpg')
 anpr.find(img)
